0e1c2fce81f3bff53dee3a88cd8aad12e6a3aa3d
Seho_Si <sunnyseho01@gmail.com>
2024-09-26 23:01:56 +0900

    10으로 나눈 나머지가 10 미만이면 그대로 숫자를 넣지만, 10으로 나눈 나머지가 10을 넘으면 아스키코드 연산을 통해 알파벳으로 변환한다. 한 차례 변환이 끝난 후엔 아스키코드 변환한 알파벳으로 나눠서 모수를 줄여나간다.

---
+++ backjoon_general_mathmathics/n11005.cpp
@@ -0,0 +1,44 @@
+//문제
+//10진법 수 N이 주어진다.이 수를 B진법으로 바꿔 출력하는 프로그램을 작성하시오.
+//
+//10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다.이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+//
+//A: 10, B : 11, ..., F : 15, ..., Y : 34, Z : 35
+//
+//입력
+//첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36) N은 10억보다 작거나 같은 자연수이다.
+//
+//출력
+//첫째 줄에 10진법 수 N을 B진법으로 출력한다.
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include<math.h>
+
+int main() {
+	int n ,b;
+	scanf("%d %d", &n, &b);
+	//대문자 아스키코드는 65~90
+	char result[100];
+    int i = 0;
+	
+    while (n > 0) {
+        int cal = n % b;
+        if (cal >= 10) {
+            result[i] = cal + 55;
+        }
+        else {
+            result[i] = cal + '0';
+        }
+        i++;
+        n /= b;
+        /*n=n/B, 이렇게 해야 n이 몫으로 새로 정의되면서
+        반복문에서 제대로 계산을 함.*/
+    }
+
+    for (int j = i - 1; j >= 0; j--) {
+        printf("%c", result[j]);
+    }
+    /*int i가 배열 ans의 길이가 되므로*/
+    return 0;
+}
--- backjoon_general_mathmathics/n2745.cpp
+++ backjoon_general_mathmathics/n2745.cpp
@@ -1,38 +1,38 @@
-//문제
-//B진법 수 N이 주어진다.이 수를 10진법으로 바꿔 출력하는 프로그램을 작성하시오.
+////문제
+////B진법 수 N이 주어진다.이 수를 10진법으로 바꿔 출력하는 프로그램을 작성하시오.
+////
+////10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다.이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+////
+////A: 10, B : 11, ..., F : 15, ..., Y : 34, Z : 35
+////
+////입력
+////첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36)
+////
+////B진법 수 N을 10진법으로 바꾸면, 항상 10억보다 작거나 같다.
+////
+////출력
+////첫째 줄에 B진법 수 N을 10진법으로 출력한다.
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<string.h>
+//#include<math.h> //pow 함수 사용하기 위해 헤더파일 추가
 //
-//10진법을 넘어가는 진법은 숫자로 표시할 수 없는 자리가 있다.이런 경우에는 다음과 같이 알파벳 대문자를 사용한다.
+//int main() {
+//	int B;
+//	char N[100];
+//	scanf("%s %d", N, &B);
+//	int result = 0;
+//	//대문자 아스키코드는 65~90
 //
-//A: 10, B : 11, ..., F : 15, ..., Y : 34, Z : 35
-//
-//입력
-//첫째 줄에 N과 B가 주어진다. (2 ≤ B ≤ 36)
-//
-//B진법 수 N을 10진법으로 바꾸면, 항상 10억보다 작거나 같다.
-//
-//출력
-//첫째 줄에 B진법 수 N을 10진법으로 출력한다.
-#define _CRT_SECURE_NO_WARNINGS
-#include<stdio.h>
-#include<string.h>
-#include<math.h> //pow 함수 사용하기 위해 헤더파일 추가
-
-int main() {
-	int B;
-	char N[100];
-	scanf("%s %d", N, &B);
-	int result = 0;
-	//대문자 아스키코드는 65~90
-
-	for (int i = 0; i < strlen(N); i++) {
-		int temp = 0; //임시로 저장할 변수
-		if (N[i] >= 65 && N[i] <= 90) {
-			temp = N[i] - 55;
-		}
-		else if (N[i] >= '0' && N[i] <= '9') {
-			temp = N[i] - '0';
-		}
-		result += temp * pow(B, strlen(N) - i - 1); //pow : 진법 변환 함수
-	}
-	printf("%d", result);
-}
+//	for (int i = 0; i < strlen(N); i++) {
+//		int temp = 0; //임시로 저장할 변수
+//		if (N[i] >= 65 && N[i] <= 90) {
+//			temp = N[i] - 55;
+//		}
+//		else if (N[i] >= '0' && N[i] <= '9') {
+//			temp = N[i] - '0';
+//		}
+//		result += temp * pow(B, strlen(N) - i - 1); //pow : 진법 변환 함수
+//	}
+//	printf("%d", result);
+//}
